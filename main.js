302c760ddc3f7c04fcd1153161ae3b0f5fc9be36
Purvik Patel <purvikpatel619@gmail.com>
2021-05-17 09:20:38 +0530

    All recent changes

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 const rclnodejs = require('rclnodejs')
-
 //let count = 1
 
-//app.allowRendererProcessReuse = false
+app.allowRendererProcessReuse = false
 
 function createWindow () {
   // Create the browser window.
@@ -30,10 +29,36 @@ function createWindow () {
       //console.log(count)
       //count += 1
     });
-    node.createSubscription('sensor_msgs/msg/Image', '/rexrov/camera/image_raw', (m) => {
+    node.createSubscription('sensor_msgs/msg/Image', '/rexrov/camera/image_raw', {isRaw : true }, (m) => {
         //image = msg['data']
-        console.log("msg received")
+        img = new Uint8ClampedArray(m);
+        img = img.slice(64);
+        // 492 768 2304
+        //console.log(img);
+        win.webContents.send('image',img);
+
     })
+    node.createSubscription('sensor_msgs/msg/Image', '/rexrov/cameraright/image_raw', {isRaw : true }, (camera2) => {
+      //image = msg['data']
+      img2 = new Uint8ClampedArray(camera2);
+      img2 = img2.slice(72);
+      // 492 768 2304
+      //console.log(img);
+      win.webContents.send('image2',img2);
+
+  })
+  node.createSubscription('uuv_sensor_ros_plugins_msgs/msg/DVL', '/rexrov/dvl', (dvl) => {
+    //image = msg['data']
+    //console.log(velocity);
+    win.webContents.send('velocity', dvl);
+
+})
+node.createSubscription('sensor_msgs/msg/FluidPressure', '/rexrov/pressure', (pressure) => {
+  //image = msg['data']
+  //console.log(velocity);
+  win.webContents.send('pressure', pressure);
+
+})
     node.spin();
     });
     
